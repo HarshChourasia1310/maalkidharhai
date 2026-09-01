46df54b3ded92de74b4a66686e0855a724861230
Harsh Chourasia <132496953+HarshChourasia1310@users.noreply.github.com>
2026-09-01 23:29:16 +0530

    v1 of share my money tracking

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE = "khata-v1";
+var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }).then(function(){ return self.skipWaiting(); }));
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () { return caches.match(e.request).then(function(r){ return r || caches.match("./index.html"); }); })
+  );
+});
