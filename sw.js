828f816b6550de2cb20e6a1d8a2b77b89bff629f
Harsh Chourasia <132496953+HarshChourasia1310@users.noreply.github.com>
2026-09-02 00:40:42 +0530

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "khata-v2";
+var CACHE = "khata-v3";
 var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", function (e) {
